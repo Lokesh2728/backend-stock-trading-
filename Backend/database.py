c378428a057bd7305c88a3b6dcaf4be206a8b9f3
Lokesh Patel <101927859+Lokesh2728@users.noreply.github.com>
2026-04-21 11:48:47 +0530

    Refactor database.py for improved readability

--- Backend/database.py
+++ Backend/database.py
@@ -1,16 +1,22 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
+load_dotenv()
 
-DATABASE_URL = "mysql+pymysql://root:Lokesh%4027%24%23@localhost:3306/trading_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True
 )
 
-SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
 
@@ -20,4 +26,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
